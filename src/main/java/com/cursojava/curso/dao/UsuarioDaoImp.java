a0af1a1d43a95e2f0b626c459a7578ceb8408c0c
Madzilla <cryptomtz@hotmail.com>
2023-05-26 17:54:39 -0600

    Mensaje de commit

---
+++ src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
@@ -0,0 +1,25 @@
+package com.cursojava.curso.dao;
+
+
+import com.cursojava.curso.models.Usuario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional
+public class UsuarioDaoImp implements UsuarioDao{
+
+    @PersistenceContext
+    private EntityManager entityManager;
+    @Override
+    @Transactional
+    public List<Usuario> getUsuarios() {
+        String query = "FROM Usuario";
+        List<Usuario> resultado = entityManager.createQuery(query).getResultList();
+        return resultado;
+    }
+}
